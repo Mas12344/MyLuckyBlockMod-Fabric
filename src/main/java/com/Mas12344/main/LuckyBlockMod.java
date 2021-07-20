ee3a08804120fd5e9dc85e4179d0b9ed6141d084
Mas12344 <grzybek2002@gmail.com>
2021-07-20 12:52:06 +0200

    Setting things up

---
+++ src/main/java/com/Mas12344/main/LuckyBlockMod.java
@@ -0,0 +1,10 @@
+package com.Mas12344.main;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LuckyBlockMod implements ModInitializer{
+    @Override
+    public void onInitialize(){
+        System.out.println("I'm feeling lucky!");
+    }
+}
