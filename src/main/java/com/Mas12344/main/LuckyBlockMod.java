045a63360bfdac7ad1ab77a7aafac12e2a60bba6
Mas12344 <grzybek2002@gmail.com>
2021-07-20 17:51:10 +0200

    Addel block model and textures

--- src/main/java/com/Mas12344/main/LuckyBlockMod.java
+++ src/main/java/com/Mas12344/main/LuckyBlockMod.java
@@ -1,10 +1,34 @@
 package com.Mas12344.main;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 
 public class LuckyBlockMod implements ModInitializer{
+
+    public static final Block Lucky_Block = new Block(FabricBlockSettings.of(Material.SOLID_ORGANIC).strength(1.0f));
+
     @Override
     public void onInitialize(){
         System.out.println("I'm feeling lucky!");
+        Registry.register(Registry.BLOCK,
+                new Identifier("lb_mod", "lucky_block"),
+                Lucky_Block
+        );
+        Registry.register(Registry.ITEM,
+                new Identifier("lb_mod", "lucky_block"),
+                new BlockItem(Lucky_Block, new FabricItemSettings().group(ItemGroup.MISC))
+        );
+
+
     }
+
+
 }
